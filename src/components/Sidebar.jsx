d33efd2080caa6950d382a8e8a61a37ff3ba63e2
Julie <jc.lauesen@gmail.com>
2023-09-26 13:16:21 +0200

    i gang med at sætte logo ind i sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+
+import shoppingCart from "./img_171562-3516626141.png"
+
 export default function Sidebar(props) {
-
-
     return (
         <div>
             <nav className="nav flex-column">
+                <img src="{shoppingCart}" alt="shoppingcartlogo" />
+
+
                 <a className="nav-link" href="#">Indkøbskurv</a>
                 <a className="nav-link" href="#">Produkter</a>
                 <button>Betal</button>
