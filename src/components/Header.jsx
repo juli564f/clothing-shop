e8c3a695ef129ddea7a1084fca07e04940bdbe8d
Julie <jc.lauesen@gmail.com>
2023-09-26 11:33:46 +0200

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(props) {
+    return( 
+        <nav className="navbar">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Phone - Book
+          </a>
+        </div>
+      </nav>
+    )
+}
