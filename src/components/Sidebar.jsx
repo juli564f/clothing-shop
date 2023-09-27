1edfffd6d144e61e4e4805fe6082f4696ce3e5be
Monique-Artfull <134688072+Monique-Artfull@users.noreply.github.com>
2023-09-27 14:36:29 +0100

    Carousel3

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 
 import shoppingCart from "./img_171562-3516626141.png"
 
-export default function Sidebar(props) {
+/*export default function Sidebar(props) {
     return (
         <div>
             <nav className="nav flex-column">
-                <img className="shoppingcart" src={shoppingCart} alt="shoppingcartlogo" />
+                <img  src={shoppingCart} alt="shoppingcartlogo" style={{ width: '20px' }} />
 
                 <a className="nav-link" href="#">Indkøbskurv</a>
                 <a className="nav-link" href="#">Produkter</a>
@@ -18,4 +20,33 @@ export default function Sidebar(props) {
             </nav>
         </div>
     )
+}*/
+
+
+
+function Example() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
 }
+
+export default Example
