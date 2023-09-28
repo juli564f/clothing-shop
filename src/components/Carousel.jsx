e8637f5109bd01e1517928c4dc1b6e95ac8dfdce
Julie <jc.lauesen@gmail.com>
2023-09-28 11:10:36 +0200

    cards

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,59 +1,62 @@
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Carousel from 'react-bootstrap/Carousel';
-import { CarouselItem } from 'react-bootstrap';
 import "../components/carousel.css"
 
 
-import Hoodie from "./hoodie.png"
+//Dette er billederne til de forskellige kort som er importeret. 
+import Hoodie from "./hoodie-png-4-3601372402.png"
 import Pants from "./bukser.png"
 import Shirt from "./shirt.png"
 
-
+//Button importeret
 import Button from 'react-bootstrap/Button';
+
+//Card importeret. 
 import Card from 'react-bootstrap/Card';
+
+
 
 function BasicExample() {
   return (
+    //Jeg har lavet div til alle kortene sammen som hedder "cards"
     <div className='cards'>
-      <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={Hoodie} alt="Hoodie" />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
 
+      {/* Dette er kort 1 med Hoodien  */}
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={Hoodie} alt="Hoodie" />
+          <Card.Body>
+            <Card.Title>Hoodie</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Add to cart</Button>
+          </Card.Body>
+        </Card>
+
+      {/* Dette er kort 2 med pants.  */}
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={Pants} alt="Pants" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Pants</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">Add to cart</Button>
         </Card.Body>
       </Card>
 
+      {/* Dette er kort 3 med t shirt.  */}
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={Shirt} alt="Shirt" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>T-shirt</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
+          <Button variant="primary">Add to cart</Button>
         </Card.Body>
-      </Card>
-
-
-    
+      </Card>    
     </div>
 
 
