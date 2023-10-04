ab664ba63a71f8547daadc3a05abb3ed4ab50a48
Monique-Artfull <134688072+Monique-Artfull@users.noreply.github.com>
2023-10-04 13:02:58 +0100

    commentar

--- src/components/Carditem.jsx
+++ src/components/Carditem.jsx
@@ -7,13 +7,17 @@ import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 
 //Dette er vores skabelon til vores kort som indeholder tøjet.
+// functionen Carditem er kortets objekter, objektet er "item, 
+// removeFromList skal fjerne objektet, addToCart skal flytte objektet til indkøbskurv
+//function af onClick "handleAddToCart"
 export default function Carditem({item, removeFromList, addToCart}) {
 
   function handleAddToCart(){
     addToCart(item);
     removeFromList(item);
   };
-
+//return indhold af Carditem. her er opsætningen af objektet (item) aka Card
+//under Card har vi elementerne Img, Body, Title, Text, Button
   return (
     <Card style={{ width: '18rem',  margin:'20px'}}>
         <Card.Img variant="top" src={item.img} alt="Hoodie" />
